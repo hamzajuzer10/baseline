7f9e4e205368d78f710460b202fce45978d0366d
simanli12 <56723489+simanli12@users.noreply.github.com>
2020-01-07 11:05:29 +0000

    update

--- baseline_query.py
+++ baseline_query.py
@@ -15,7 +15,7 @@ def baseline_dashboard(project_id, dataset_id):
         
         promo_dashboard_sql = """
       with unnest_tb AS(
-SELECT distinct promo_id, promo_year, sku_root_id, promo_mechanic, discount_depth, no_to_pay, no_to_buy, end_date,
+SELECT distinct promo_id, promo_year, sku_root_id, promo_mechanic, discount_depth, end_date,
                 DATE_ADD(DATE_TRUNC(end_date, WEEK(MONDAY)), INTERVAL 7 DAY) AS promo_bline_end_date, store_ids
 FROM `ETL.aggregate_promo_to_sku_summary`
 CROSS JOIN UNNEST(store_ids) as store_ids),
@@ -40,14 +40,12 @@ post_promo AS(
  SELECT 
 date, promo_id, CAST(promo_year AS STRING) AS promo_year, sku_root_id, promo_mechanic, 
 IFNULL(discount_depth, "ISNULL") AS discount_depth, 
-IFNULL(CAST(no_to_pay AS STRING), "ISNULL") AS no_to_pay, 
-IFNULL(CAST(no_to_buy AS STRING), "ISNULL") AS no_to_buy,
 sum(s_fw_bl_qty) as s_fw_bl_qty,
 sum(s_fw_bl_sale) as s_fw_bl_sale, 
 sum(s_fw_bl_margin) as s_fw_bl_margin, 
 sum(s_fw_bl_discount) as s_fw_bl_discount
 FROM store_level
-GROUP BY promo_id, promo_year, sku_root_id, promo_mechanic, discount_depth, no_to_pay, no_to_buy, date),
+GROUP BY promo_id, promo_year, sku_root_id, promo_mechanic, discount_depth, date),
 --        post_promo as(select *from baseline.post_promo),
 
 
@@ -56,8 +54,6 @@ GROUP BY promo_id, promo_year, sku_root_id, promo_mechanic, discount_depth, no_t
         cast(DATE(date) AS DATE) AS date,
         uniq_id, sku_root_id, promo_id, promo_year, promo_mechanic, 
         IFNULL(discount_depth, 'ISNULL') AS discount_depth, 
-        IFNULL(no_to_pay, 'ISNULL') AS no_to_pay, 
-        IFNULL(no_to_buy, 'ISNULL') AS no_to_buy,
         CAST(change_flag AS NUMERIC) AS change_flag,
         CAST(total_sale_qty AS NUMERIC) as tt_sale_qty_ref,
         CAST(sale_qty_bl AS NUMERIC) AS sale_qty_bl,
@@ -65,7 +61,7 @@ GROUP BY promo_id, promo_year, sku_root_id, promo_mechanic, discount_depth, no_t
         FROM `baseline.baseline` ),
 
         agg_table as (
-        SELECT date,  uniq_id, sku_root_id, promo_id, promo_year, promo_mechanic, discount_depth, no_to_pay, no_to_buy,
+        SELECT date,  uniq_id, sku_root_id, promo_id, promo_year, promo_mechanic, discount_depth,
                change_flag, tt_sale_qty_ref, sale_qty_pct,
                CASE WHEN change_flag = 3 THEN post_promo.s_fw_bl_qty ELSE promo.total_sale_qty END AS tt_sale_qty,
                CASE WHEN change_flag = 3 THEN post_promo.s_fw_bl_sale ELSE promo.total_sale_amt END AS tt_sale_amt,
@@ -73,17 +69,15 @@ GROUP BY promo_id, promo_year, sku_root_id, promo_mechanic, discount_depth, no_t
                CASE WHEN change_flag = 3 THEN post_promo.s_fw_bl_discount ELSE promo.total_discount_amt END AS tt_discount,
                sale_qty_bl, sale_qty_bl * std.std_price_per_unit AS sale_amt_bl, sale_qty_bl * std.margin_per_unit AS margin_amt_bl,            
                avg_bline_qty * std.std_price_per_unit AS avg_bline_sale, avg_bline_qty * std.margin_per_unit AS avg_bline_margin,
-               promo.* EXCEPT (date, sku_root_id, promo_id, promo_year, promo_mechanic, discount_depth, no_to_pay, no_to_buy, std_price)
+               promo.* EXCEPT (date, sku_root_id, promo_id, promo_year, promo_mechanic, discount_depth, std_price)
         FROM baseline bl
         LEFT JOIN post_promo
-        USING (date, sku_root_id, promo_id, promo_year, promo_mechanic, discount_depth, no_to_pay, no_to_buy)
-        LEFT JOIN (SELECT * EXCEPT (store_ids, promo_year, discount_depth, no_to_pay, no_to_buy), 
+        USING (date, sku_root_id, promo_id, promo_year, promo_mechanic, discount_depth)
+        LEFT JOIN (SELECT * EXCEPT (store_ids, promo_year, discount_depth), 
                     CAST(promo_year AS STRING) AS promo_year, 
-                    IFNULL(discount_depth, 'ISNULL') AS discount_depth, 
-                    IFNULL(CAST(no_to_pay AS STRING), 'ISNULL') AS no_to_pay, 
-                    IFNULL(CAST(no_to_buy  AS STRING), 'ISNULL') AS no_to_buy
+                    IFNULL(discount_depth, 'ISNULL') AS discount_depth
                    FROM `ETL.aggregate_promo_to_sku_summary`) promo
-        USING (date, sku_root_id, promo_id, promo_year, promo_mechanic, discount_depth, no_to_pay, no_to_buy)
+        USING (date, sku_root_id, promo_id, promo_year, promo_mechanic, discount_depth)
         LEFT JOIN `ETL.aggregate_std_price_margin` std
         USING (sku_root_id)),
         
@@ -94,13 +88,13 @@ GROUP BY promo_id, promo_year, sku_root_id, promo_mechanic, discount_depth, no_t
         WHERE change_flag <>3),
         
         final_tb as (
-        SELECT date, sku_root_id, promo_id, promo_year, promo_mechanic, discount_depth, no_to_pay, no_to_buy, 
+        SELECT date, sku_root_id, promo_id, promo_year, promo_mechanic, discount_depth, 
                change_flag, includes_weekend, tt_sale_qty_ref, sale_qty_pct, tt_sale_qty, tt_sale_amt, tt_margin_amt, tt_discount,
                sale_qty_bl, sale_amt_bl, margin_amt_bl, avg_bline_qty, avg_bline_sale, avg_bline_margin, total_sale_amt as promo_sale_amt, total_margin_amt as promo_margin_amt, total_sale_qty as promo_sale_qty, s_prev_bl_qty, pf_after_bl_qty, total_discount_amt,
-               uniq_id_table.* EXCEPT(sku_root_id, promo_id, promo_year, promo_mechanic, discount_depth, no_to_pay, no_to_buy)
+               uniq_id_table.* EXCEPT(sku_root_id, promo_id, promo_year, promo_mechanic, discount_depth)
         FROM agg_table
         LEFT JOIN uniq_id_table
-        USING (sku_root_id, promo_id, promo_year, promo_mechanic, discount_depth, no_to_pay, no_to_buy)),
+        USING (sku_root_id, promo_id, promo_year, promo_mechanic, discount_depth)),
         
       baseline_promo as (
       SELECT * EXCEPT(includes_weekend),
@@ -122,7 +116,7 @@ GROUP BY promo_id, promo_year, sku_root_id, promo_mechanic, discount_depth, no_t
 
       store_mapping as (
                     SELECT distinct sku_root_id, promo_id, CAST(promo_year AS STRING) AS promo_year, promo_mechanic, IFNULL(discount_depth, "ISNULL") as discount_depth, 
-                    IFNULL(no_to_pay, "ISNULL") as no_to_pay, IFNULL(no_to_buy, "ISNULL") as no_to_buy, store_id
+                     store_id
                     FROM `ETL.aggregate_promo_to_sku_summary` , UNNEST(store_ids) as store_id),
       store_flag as (
                     SELECT DISTINCT date, sku_root_id, store_id, 1 AS store_promo_flag
@@ -130,17 +124,18 @@ GROUP BY promo_id, promo_year, sku_root_id, promo_mechanic, discount_depth, no_t
       
       bl as (
               SELECT date, uniq_id,  sum(store_promo_flag) as store_promo_flag
-              from `baseline.baseline_promo` 
+              from baseline_promo 
               LEFT JOIN store_mapping
-              USING (sku_root_id, promo_id, promo_year, promo_mechanic, discount_depth, no_to_pay, no_to_buy)
+              USING (sku_root_id, promo_id, promo_year, promo_mechanic, discount_depth)
               left join store_flag
               using (date, sku_root_id, store_id)
               GROUP BY date, uniq_id)
       
 SELECT promo.*, store_promo_flag
-from `baseline.baseline_promo` promo
+from baseline_promo promo
 LEFT JOIN bl
 USING (date, uniq_id)
+
         """
         
         # Create a disctionary to loop over all destination tables and scripts
