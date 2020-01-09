78ceb26be49eb89734684ec6f0160378ce8c329a
Anthony Tsoi <antsoi@deloitte.co.uk>
2020-01-09 09:11:19 +0000

    dask reset_index after read_csv

--- unzip_file.py
+++ unzip_file.py
@@ -161,6 +161,7 @@ def csv_checks(csv_filename, dataset_schema):
         full_csv_data = dd.read_csv(
             csv_filename, header=None, sep="|", engine="python", assume_missing=True, dtype="str"
         )
+        full_csv_data.reset_index(drop=True)
         logger.info("csv file: {} loaded to dataframe".format(csv_filename))
         logger.info(csv_data.head())
         logger.info(full_csv_data.head())
