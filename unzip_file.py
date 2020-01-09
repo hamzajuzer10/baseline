994a10acfa7b7449d0470963247d44ceda68a78a
Anthony Tsoi <antsoi@deloitte.co.uk>
2020-01-09 12:17:28 +0000

    update dask read_csv with quotechar='"', error_bad_lines=False

--- unzip_file.py
+++ unzip_file.py
@@ -159,7 +159,14 @@ def csv_checks(csv_filename, dataset_schema):
             csv_filename, header=None, index_col=False, sep="|", engine="python", nrows=10
         )
         full_csv_data = dd.read_csv(
-            csv_filename, header=None, sep="|", engine="python", assume_missing=True, dtype="str"
+            csv_filename,
+            header=None,
+            sep="|",
+            engine="python",
+            assume_missing=True,
+            dtype="str",
+            quotechar='"',
+            error_bad_lines=False,
         )
 
         logger.info("csv file: {} loaded to dataframe".format(csv_filename))
