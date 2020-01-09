0d1be5776acbf284294e0f9c8f5b22e42909a33c
Anthony Tsoi <antsoi@deloitte.co.uk>
2020-01-09 09:27:26 +0000

    check if index[0] == 0

--- unzip_file.py
+++ unzip_file.py
@@ -161,7 +161,8 @@ def csv_checks(csv_filename, dataset_schema):
         full_csv_data = dd.read_csv(
             csv_filename, header=None, sep="|", engine="python", assume_missing=True, dtype="str"
         )
-        full_csv_data = full_csv_data.reset_index()
+        if full_csv_data.index[0] != 0:
+            full_csv_data = full_csv_data.reset_index()
         logger.info("csv file: {} loaded to dataframe".format(csv_filename))
         logger.info(csv_data.head())
         logger.info(full_csv_data.head())
