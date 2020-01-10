db2238aefa40d28dae090aa805c0222bf7af2a65
Anthony Tsoi <antsoi@deloitte.co.uk>
2020-01-10 17:25:26 +0000

    test

--- bq_upload_test.py
+++ bq_upload_test.py
@@ -20,7 +20,7 @@ job_config.source_format = bigquery.SourceFormat.CSV
 job_config.autodetect = True
 job_config.write_disposition = "WRITE_TRUNCATE"
 job_config.skip_leading_rows = 1
-job_config.field_delimiter = ","
+job_config.field_delimiter = "|"
 
 # schema = [SF("col1", "STRING"), SF("two", "STRING"), SF("three", "STRING"), SF("four", "STRING")]
 # schema = [SF(a, b), SF("two", "STRING"), SF("three", "STRING"), SF("four", "STRING")]
