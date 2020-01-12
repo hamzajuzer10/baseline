c72366b776a7d585c436db7a0fd05b9cbffee7ff
Anthony Tsoi <antsoi@deloitte.co.uk>
2020-01-12 12:04:01 +0000

    switch to eroski-deltas gcp storage location

--- delta_etl.py
+++ delta_etl.py
@@ -59,7 +59,7 @@ project_id = "gum-eroski-dev"
 # define read GCP parameters
 dataset_id = "source_data"
 bucket = "erk-data-feed"
-storage_filepath = "Working_folder/AT/ETL_test/"
+storage_filepath = "eroski-deltas/20191205/"
 blobs = storage_client.list_blobs(bucket, prefix=storage_filepath)
 blob_list = [blob.name for blob in blobs]
 
